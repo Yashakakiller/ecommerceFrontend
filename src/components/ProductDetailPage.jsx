390d9e272cad02390ff3a49b3776e1287e2d5cd2
unknown <yashakakiller@gmail.com>
2023-06-14 17:59:55 +0530

    client changes

--- src/components/ProductDetailPage.jsx
+++ src/components/ProductDetailPage.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+
 
 const ProductsDetailPage = () => {
+
+  const {id} = useParams()
+
+  const [Product, setProduct] = useState([])
+
+  useEffect(()=>{
+      fetchData()
+  },[])
+
+
+  const fetchData = async () => {
+    const backend = await axios.get(`https://ecommerce-backend-vyn1.onrender.com/products/product/${id}`)
+    console.log(backend.data)
+    setProduct(backend.data)
+  }
+
   return (
     <div>ProductsDetailPage</div>
   )
