29c36c9a985899c3783d7be896321ad6545e2333
unknown <yashakakiller@gmail.com>
2023-07-17 10:53:02 +0530

    client

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import {Link} from 'react-router-dom'
+import {API_CALL} from '../api'
+
 
 
 const NewsLetter = () => {
