416f2a45afe14d33af55450d0020ac21ed7e07c3
unknown <yashakakiller@gmail.com>
2023-07-17 10:44:41 +0530

    client

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -4,7 +4,27 @@ import {Link} from 'react-router-dom'
 
 
 const NewsLetter = () => {
-    const [userNewsEmail , setUserNewsEmail] = useState('')
+    const [formData , setFormData] = useState({
+        email:"",
+        name:""
+    })
+
+
+    const handleSubmit = async() => {
+        e.preventDefault()
+        const data = await axios.post(`${API_CALL}/contact/specificProduct`,formData)
+        setFormData({
+            email:"",
+            name:""
+        })
+    }
+
+
+    const handleChange = (e) =>{
+        setFormData({...formData,[e.target.name]:e.target.value})
+    }
+
+
   return (
     <>
         <section id="newsletter" className='section-p1' style={{margin:"0 auto",width:"95%",borderRadius:"30px"}}>
@@ -12,12 +32,13 @@ const NewsLetter = () => {
                 <h4>Sign Up for NewsLetter</h4>
                 <p>Get Email updates about our latest products and offers.</p>
             </div>
-            <div className="newsform">
-                <input type="text"  placeholder='Your E-mail Address' value={userNewsEmail} onChange={(e) => setUserNewsEmail(e.target.value)}/>
+            <form className="newsform" onSubmit={handleSubmit}>
+                <input type="text"  placeholder='Your Name' value={formData.name} name='name' onChange={(e) => handleChange(e)}/>
+                <input type="text"  placeholder='Your E-mail Address' value={formData.email} name='email' onChange={(e) => handleChange(e)}/>
                 <button>Sign Up</button>
-            </div>
+            </form>
         </section>
-    </>
+        </>
   )
 }
 
