962b89224d7af744e8ac02d6e2b2d4dd3e393454
unknown <yashakakiller@gmail.com>
2023-07-18 22:59:29 +0530

    client

--- src/components/ProductDetailPage.jsx
+++ src/components/ProductDetailPage.jsx
@@ -139,7 +139,7 @@ theme="dark"
           </div>
 
           <div className="col-lg-6 col-md-12 col-12 p-desc">
-            <h6>{product.category}</h6>
+            {/* <h6>{product.category}</h6> */}
             <h3 className="py-4">{product.name}</h3>
             <h2>₹{product.price}</h2>
             {product.quantity === 0 || product.quantity === null ? (
