9d921dbdc8ce5e6eec933b5ef12a1f4e91a42972
unknown <yashakakiller@gmail.com>
2023-07-27 02:11:28 +0530

    client

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -3,44 +3,44 @@ import axios from 'axios'
 import { API_CALL } from '../api'
 import { useNavigate } from 'react-router-dom'
 
-
 const NewArrivals = () => {
-    const [products , setProducts] = useState([])
-    const navigate = useNavigate()
+  const [products, setProducts] = useState([])
+  const navigate = useNavigate()
 
-    const fetchProducts = async() =>{
-        const products = await axios.get(`${API_CALL}/products/newArrivals`);
-        setProducts(products.data.products)
-        
-    }
-    useEffect(()=>{
-        fetchProducts()
-    },[])
-// console.log(products)
+  const fetchProducts = async () => {
+    const response = await axios.get(`${API_CALL}/products/newArrivals`);
+    setProducts(response.data.products)
+  }
 
+  useEffect(() => {
+    fetchProducts()
+  }, [])
 
-const productOpen = async(pId) => {
-  navigate(`/product/${pId}`)
-}
-
+  const productOpen = async (pId) => {
+    navigate(`/product/${pId}`)
+  }
 
   return (
     <>
-       <section id="newProducts" >
-      <h2>New Arrivals</h2>
-      <p>Get Ready for Trends!</p>
-      <div className="new_container">
-        {products.map((data) => (
-          <div className="new_box" key={data._id}>
-            <img src={data.img} alt='dataured image logo' onClick={() => productOpen(data._id)} />
-            <div className="desc">
-              {data.name.length > 90 ? (<h5>{data.name.slice(0 , 85)}...</h5>) : (<h5>{data.name}</h5>)}
-              {data.quantity === 0 ? (<h5 className='text-danger'>OOPS! Out of Stock</h5>) : (<h5>Quantity Available - {data.quantity}</h5>)}
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+      <section id="newProducts" className='my-4'>
+        <h2>New Arrivals</h2>
+        <p>Get Ready for Trends!</p>
+        <div className="new_container">
+          {products.length > 0 ? (
+            products.map((data) => (
+              <div className="new_box" key={data._id}>
+                <img src={data.img} alt='dataured image logo' onClick={() => productOpen(data._id)} />
+                <div className="desc">
+                  {data.name.length > 90 ? (<h5>{data.name.slice(0, 85)}...</h5>) : (<h5>{data.name}</h5>)}
+                  {data.quantity === 0 ? (<h5 className='text-danger'>OOPS! Out of Stock</h5>) : (<h5>Quantity Available - {data.quantity}</h5>)}
+                </div>
+              </div>
+            ))
+          ) : (
+              <h3 className='text-center text-danger'>No New Products Arrived..</h3>
+            )}
+        </div>
+      </section>
     </>
   )
 }
