0966f40621a49acef8c05653be308f43e3ed8f59
unknown <yashakakiller@gmail.com>
2023-07-27 01:54:06 +0530

    client

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
         e.preventDefault();
 
         const backend = await axios.post(`${API_CALL}/accounts/user/auth/login` ,data);
-
+        console.log(backend.data.message)
         if(backend.data.success === false){
             return alert(backend.data.message)
         }
@@ -24,7 +24,7 @@ const Login = () => {
         navigate(`/accounts/profile/user/${backend.data.user._id}`) 
 
     }
-
+    
     const valueHandler = (e) => {
         setData({...data , [e.target.name]:e.target.value})
     }
