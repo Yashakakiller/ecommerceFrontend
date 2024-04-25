32e5f8ee7f960db2b673c1acacc4979ea36f1971
Yash Gupta <yashakakiller@gmail.com>
2024-04-25 23:34:57 +0530

    changes

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-// export const API_CALL = "https://ecommerce-backend-h5mu.onrender.com"
-export const API_CALL = "http://localhost:8080"
+export const API_CALL = "https://ecommerce-backend-h5mu.onrender.com"
+// export const API_CALL = "http://localhost:8080"
